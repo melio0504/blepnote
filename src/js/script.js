71e360527f48243ef3105ec74aa61e43dd4ecd29
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-03-31 23:33:24 +0800

    Add functionalities in FAQ section

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,13 @@ tailwind.config = {
         }
     }
 }
+
+document.querySelectorAll('#faq button').forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling;
+        const icon = button.querySelector('svg');
+        
+        answer.classList.toggle('hidden');
+        icon.classList.toggle('rotate-180');
+    });
+});
