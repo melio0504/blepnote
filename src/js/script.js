cbd952a48538d922afa60340a079586c39c00fb0
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-03-31 23:34:04 +0800

    Add STP script function

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,16 @@ document.querySelectorAll('#faq button').forEach(button => {
         icon.classList.toggle('rotate-180');
     });
 });
+
+// Scroll-To-Top Button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        scrollToTopBtn.classList.remove("opacity-0", "invisible", "translate-y-4");
+        scrollToTopBtn.classList.add("opacity-100", "visible", "translate-y-0");
+    } else {
+        scrollToTopBtn.classList.add("opacity-0", "invisible", "translate-y-4");
+        scrollToTopBtn.classList.remove("opacity-100", "visible", "translate-y-0");
+    }
+});
